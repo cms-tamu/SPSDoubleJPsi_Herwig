439343fa6ee2c44d0012cda774079f717bcc6d3b
Jamal <jrorie@rice.edu>
2016-04-13 16:38:42 -0500

    Updated contents of CRAB script for CRAB3

--- SPS_2JPsi_4mu_13TeV_herwigPP_CMSSW763/SPS_2JPsi_4mu_13TeV_herwigPP_CMSSW763_batch001_CRAB3_GEN-SIM_v01.py
+++ SPS_2JPsi_4mu_13TeV_herwigPP_CMSSW763/SPS_2JPsi_4mu_13TeV_herwigPP_CMSSW763_batch001_CRAB3_GEN-SIM_v01.py
@@ -1,38 +1,26 @@
-[CRAB]
-jobtype     = cmssw
-scheduler   = pbs
-use_server  = 0
+from WMCore.Configuration import Configuration
+config = Configuration()
 
-#[CONDORG]
-#globus_rsl = (queue=grid)(maxWalltime=1440)(maxCpuTime=1440)
+config.section_("General")
+config.General.requestName = 'SPS_2JPsi_4mu_13TeV_herwigPP_CMSSW763_batch001_GEN-SIM_v01'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
 
-[PBS]
-queue     = hepxrt
-resources = cput=12:00:00,walltime=12:00:00
+config.section_("JobType")
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = 'SPS_2JPsi_4mu_13TeV_herwigPP_CMSSW763_batch001__cfi__GEN-SIM_v01.py'
+config.JobType.inputFiles = ['MEgg2JpsiJpsi.so']
 
-[GRID]
-rb                   = CERN
-proxy_server         = fg-myproxy.fnal.gov
-virtual_organization = cms
-se_white_list        = T3_US_TAMU
-ce_white_list        = T3_US_TAMU
+config.section_("Data")
+#config.Data.inputDBS = 'phys03'
+config.Data.outputPrimaryDataset = 'SPS_JPsi_RunII'
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 1000
+NJOBS = 100
+config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+config.Data.outLFNDirBase = '/store/user/jrorie/'
+config.Data.publication = True
+config.Data.outputDatasetTag = 'SPS_2JPsi_4mu_13TeV_herwigPP_CMSSW763_batch001_GEN-SIM_v01'
 
-[CMSSW]
-pset        = SPS_gg-2Jpsi-4mu_8TeV_herwigPP250_5311p5_GEN_v12.py
-output_file = SPS_gg-2Jpsi-4mu_8TeV_herwigPP250_5311p5_GEN_v12.root
-datasetpath = None
-events_per_job = 100
-number_of_jobs = 10
-
-[USER]
-thresholdLevel          = 80
-email                   = pakhotin.queue@gmail.com
-copy_data               = 1
-return_data             = 0
-storage_element         = T3_US_TAMU
-additional_input_files  = MEgg2JpsiJpsi.so
-user_remote_dir         = SPS_gg-2Jpsi-4mu_8TeV_herwigPP250_5311p5_GEN_v12
-ui_working_dir          = SPS_gg-2Jpsi-4mu_8TeV_herwigPP250_5311p5_GEN_v12
-publish_data_name       = SPS_gg-2Jpsi-4mu_8TeV_herwigPP250_5311p5_GEN_v12
-publish_data            = 1
-dbs_url_for_publication = https://cmsdbsprod.cern.ch:8443/cms_dbs_ph_analysis_01_writer/servlet/DBSServlet
+config.section_("Site")
+config.Site.storageSite = 'T3_US_FNALLPC'
